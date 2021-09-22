1853a791d21dc0aa74aabe32ab91c8dbf83e0bc5
unknown <israa_alnazer@hotmail.com>
2021-09-22 16:54:54 +0300

    add start script

--- app.js
+++ app.js
@@ -181,6 +181,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh no sth went wrong'
     res.status(statusCode).render('error', { err })
 })
-app.listen(8000, () => {
-    console.log('Listening on Port 8000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`)
 })
